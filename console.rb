94bd027dc2bfaac5d415909c9ad35b6077193838
James Monks <J.monks23@gmail.com>
2020-06-30 14:53:21 +0100

    save method created for db

--- console.rb
+++ console.rb
@@ -2,7 +2,15 @@ require("pry-byebug")
 require_relative("models/properties")
 
 
-property1 = Property.new( {"address" => "111 Princes Street", "value" => "3000", "bedrooms" => "5", "year" => "1980"} )
+property1 = Property.new( {"address" => "111 Princes Street", "value" => "27000", "bedrooms" => "5", "year" => "1980"} )
+property2 = Property.new( {"address" => "22 Oliver Gardens", "value" => "100000", "bedrooms" => "1", "year" => "1955"} )
+property3 = Property.new( {"address" => "33 Penny Lane", "value" => "3000", "bedrooms" => "3", "year" => "1923"} )
+property4 = Property.new( {"address" => "56 Helena Way", "value" => "4400", "bedrooms" => "6", "year" => "1934"} )
+
+property1.save()
+property2.save()
+property3.save()
+property4.save()
 
 
 
--- models/properties.rb
+++ models/properties.rb
@@ -1,3 +1,4 @@
+require('pg')
 class Property
 
     attr_accessor :address, :value, :bedrooms, :year
@@ -9,6 +10,16 @@ class Property
         @value = options['value'].to_i
         @bedrooms = options['bedrooms'].to_i
         @year = options['year'].to_i
+    end
+
+    def save()
+        db = PG.connect( {dbname: 'property_tracker', host: 'localhost'} )
+        sql = "INSERT INTO properties (address, value, bedrooms, year)
+        VALUES ($1, $2, $3, $4) RETURNING id"
+        values = [@address, @value, @bedrooms, @year]
+        db.prepare("save", sql)
+        @id = db.exec_prepared("save", values)[0]["id"].to_i()
+        db.close()
     end
 
 end
